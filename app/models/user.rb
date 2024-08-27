971ccf4a36625a86ac013f13f050f8ba6f805530
nemoimoi <nemoto.mikako@gmail.com>
2024-08-27 19:20:17 +0900

    チャット画面の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   has_many :room_users
   has_many :rooms, through: :room_users
+
+  validates :name, presence: true
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
 end
